a17608f8abd9e2d5dadd3f36e9d7551e3b102ccb
candeayala <36246965+candeayala@users.noreply.github.com>
2018-07-05 12:16:14 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,11 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	//var nombre = prompt("Please enter your name", "Harry Potter");
+	/*bloque de comentarios que no modifican el código y puedo usar como apuntes*/
+    var nombre;
+    nombre = prompt("Ingrese su nombre");
+    alert("su nombre es: " + nombre);
+}
+

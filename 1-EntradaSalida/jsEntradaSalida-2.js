f77eae7fc2f188fa0b621a477191beec016aafc3
candeayala <36246965+candeayala@users.noreply.github.com>
2018-07-05 11:24:48 -0300

    primer commit
    Estos son los cuatro ejercicios de la primera clase.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,10 @@
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//var nombre = prompt("Please enter your name", "Harry Potter");
+	/*bloque de comentarios que no modifican el código y puedo usar como apuntes*/
+    var nombre;
+    nombre = prompt("Ingrese su nombre");
+    alert("su nombre es: " + nombre);
 }
 

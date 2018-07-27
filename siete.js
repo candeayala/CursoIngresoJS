5a55ddb1aed0d3ca9e20c1219edde6310e2ed0a7
candeayala <36246965+candeayala@users.noreply.github.com>
2018-07-27 12:25:23 -0300

    Add files via upload

---
+++ siete.js
@@ -0,0 +1,31 @@
+function mostrar()
+{
+	var notas;
+	var sexo;
+	var acumuladoNotas=0;
+	var contadorNotas=0;
+	var contadorVarones=0;
+	var 
+
+	while ()
+	{
+		notas=prompt("ingrese notas");
+		notas=parseInt(notas);
+		while(notas<0 || notas>10)
+		{
+			notas=prompt("reingrese notas");
+		}
+		acumuladorNotas= acumuladorNotas+notas;
+		contadorNotas++;
+		sexo=prompt("ingrese sexo");
+		while(sexo != "f" && sexo != "m")
+		{
+			sexo=prompt("Reingrese sexo");
+		}
+
+		while()
+
+	}
+	
+
+}
